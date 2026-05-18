17029d2d1fea9d7013f209ef6a251a8a7bc2a747
ArtyBob <nova1hazard@gmail.com>
2026-05-18 16:29:15 +0700

    Test laravel passport

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\WebhookController;
+
+Route::middleware('auth:api')->get('/user-profile', function (Request $request) {
+    return response()->json($request->user());
+});
 
 Route::post('/payments', [PaymentController::class, 'create']);
 Route::post('/webhook/gateway', [WebhookController::class, 'handleCallback']);
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\WebhookController;
@@ -12,3 +13,14 @@ Route::get('/metrics', function() {
     return Prometheus::render();
 });
 Route::get('/metrics', [App\Http\Controllers\MetricsController::class, 'index']);
+
+
+Route::get('/get-test-token', function () {
+    // Берем первого пользователя или создаем тестового
+    $user = User::first() ?? User::factory()->create();
+
+    // Генерируем токен (метод доступен благодаря трейту HasApiTokens в User)
+    $token = $user->createToken('TestToken')->accessToken;
+
+    return response()->json(['token' => $token]);
+});
